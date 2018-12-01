66281fb1acd7735722f172e753cc31cd2c0d18bd
Tyukov Iliya <tyukovia@gmail.com>
2018-12-01 08:34:23 +0300

    Main menu template

---
+++ Sacrifice/Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quiting gamee");
+    }
+
+
+
+	public void LoadLevel(int levelNumber)
+    {
+        if (SceneManager.GetSceneByBuildIndex(levelNumber) != null)
+            SceneManager.LoadScene(levelNumber);
+        else
+            Debug.Log("no such index in build");
+    }
+}
